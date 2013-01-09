2907e45ce3f25e28df8f342def26898d9d23fac2
Jimmy Zhao <itomgit@gmail.com>
2013-01-09 17:51:13 +0800

    Test profile

--- qqcallback.php
+++ qqcallback.php
@@ -5,11 +5,26 @@ $access_token = $qc->qq_callback();
 if ( ! empty($access_token)) {
 	$openid = $qc->get_openid();
 	if ( ! empty($openid)) {
-		echo $openid;
 		$qc = new QC($access_token, $openid);
-		$ret = $qc->get_user_info();
-		echo '<pre>';
-		print_r($ret);
-		echo '</pre>';
+		$user_info = $qc->get_user_info();
+		if( ! empty($user_info)){
+			$profile = new stdClass();
+			$profile->id = $openid;
+			$profile->email = "";
+			$profile->{'first-name'} = $user_info['nickname'];
+			$profile->{'last-name'} = "";
+			$profile->{'lang'} = $this->session->userdata('lang');
+				
+			// get user head picture
+			if(isset($user_info['figureurl_2']) && ! empty($user_info['figureurl_2'])){
+				$profile->picture = $user_info['figureurl_2'];
+			}
+			echo "<pre>";
+			print_r($profile);
+			echo "</pre>";
+// 			$result = $this->user_manager->oAuthLogin($profile, "qq", $params["access_token"]);
+// 			redirect('user/dashboard/oauth_success');
+		}
 	}
 }
+exit("Failed to authenticate with QQ. Please try again");
