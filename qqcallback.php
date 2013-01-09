a2249895909f6f312fc449a949b304e890082ff5
Jimmy Zhao <itomgit@gmail.com>
2013-01-09 16:54:50 +0800

    Fix get user info.

--- qqcallback.php
+++ qqcallback.php
@@ -4,9 +4,12 @@ $qc = new QC();
 $access_token = $qc->qq_callback();
 if ( ! empty($access_token)) {
 	$openid = $qc->get_openid();
-	echo $openid;
-	$ret = $qc->get_info();
-	echo '<pre>';
-	print_r($ret);
-	echo '</pre>';
+	if ( ! empty($openid)) {
+		echo $openid;
+		$qc = new QC($openid, $access_token);
+		$ret = $qc->get_info();
+		echo '<pre>';
+		print_r($ret);
+		echo '</pre>';
+	}
 }
