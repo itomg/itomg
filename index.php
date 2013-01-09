d06fba338449f17d6c578c4880090639e6c45922
Jimmy Zhao <itomgit@gmail.com>
2013-01-09 14:53:22 +0800

    Add a index.php file then valid qq oauth meta tag.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Itomg">
+	<meta name="author" content="Jimmy Zhao">
+	
+	<meta property="qc:admins" content="34510734776147576375" />
+	
+	<title>My Dashboard - Tradesparq</title>
+</head>
+<body>
+<?php echo 'Hello world!';?>
+</body>
+</html>
